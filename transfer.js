4088571beb2eea2651416d76eaac7f40ba0ec897
ycl1216919 <36100212+ycl1216919@users.noreply.github.com>
2023-12-17 20:21:52 +0900

    Update transfer.js

--- transfer.js
+++ transfer.js
@@ -6,25 +6,25 @@ const {readFileSync} = require("fs");
 async function main() {
     const rpcEndpoint = process.env.NODE_URL;
     const privateKey = process.env.PRIVATE_KEY; //主账户私钥
-    const wallet = await DirectSecp256k1Wallet.fromKey(Buffer.from(privateKey, "hex"), "cosmos");
+    const wallet = await DirectSecp256k1Wallet.fromKey(Buffer.from(privateKey, "hex"), "celestia");
     const [account] = await wallet.getAccounts();
-    const gasPrice = GasPrice.fromString("0.025uatom");
+    const gasPrice = GasPrice.fromString("0.025utia");
     const client = await SigningStargateClient.connectWithSigner(rpcEndpoint, wallet, { gasPrice: gasPrice });
-    const balance = await client.getBalance(account.address, "uatom");
+    const balance = await client.getBalance(account.address, "utia");
     console.log(`主账户地址: ${account.address} 余额: ${parseFloat(balance.amount) / 1000000}`);
     const wallets = JSON.parse(readFileSync('cosmos_wallets.json', 'utf-8'));
     const recipients = wallets.map(wallet => wallet.address);
 
-    // 1 ATOM 等于 1000000 uatom
-    const amount = coins(1000000, "uatom");
+    // 1 TIA 等于 1000000 utia
+    const amount = coins(1000000, "utia");
     for (const recipient of recipients) {
         try {
             const fee = {
-                amount: coins(750, "uatom"),
+                amount: coins(750, "utia"),
                 gas: "150000",
             };
             const result = await client.sendTokens(account.address, recipient, amount, fee);
-            console.log(`${recipient}: 转账 ${amount.toString()} 成功: ${'https://www.mintscan.io/cosmos/tx/' + result.transactionHash}`);
+            console.log(`${recipient}: 转账 ${amount.toString()} 成功: ${'https://www.mintscan.io/celestia/tx/' + result.transactionHash}`);
         } catch (error) {
             console.error(`转账给 ${recipient} 失败: `, error);
         }
